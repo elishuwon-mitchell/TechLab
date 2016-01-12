1d1e9c6cf99e358d62cb877874666da56ec69161
template <template@dhcp153178.cet.emory.edu>
2016-01-12 15:16:50 -0500

    Initial Commit

---
+++ TechLab/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TechLab
+//
+//  Created by template on 1/12/16.
+//  Copyright © 2016 template. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TechLab/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TechLab
+//
+//  Created by template on 1/12/16.
+//  Copyright © 2016 template. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
